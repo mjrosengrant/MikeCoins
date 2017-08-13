9643b1464fe178eabd659cc9de94bdffb3d5ef6e
Mike <mike@rosengrant.me>
2017-08-13 12:56:23 -0400

    Implemented MikeCoinFactory

--- contracts/MikeCoin.sol
+++ contracts/MikeCoin.sol
@@ -9,14 +9,6 @@ import "./ERC20/StandardToken.sol";
 pragma solidity ^0.4.8;
 
 contract MikeCoin is StandardToken {
-	/* Public variables of the token */
-
-    /*
-    NOTE:
-    The following variables are OPTIONAL vanities. One does not have to include them.
-    They allow one to customise the token contract & in no way influences the core functionality.
-    Some wallets/interfaces might not even bother to look at this information.
-    */
     string public name;                   //fancy name: eg Simon Bucks
     uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
     string public symbol;                 //An identifier: eg SBX
@@ -35,7 +27,6 @@ contract MikeCoin is StandardToken {
         symbol = _tokenSymbol;                               // Set the symbol for display purposes
     }
 
-    /* Approves and then calls the receiving contract */
     function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
